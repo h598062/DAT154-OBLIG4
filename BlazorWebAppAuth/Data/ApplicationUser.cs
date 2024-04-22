b62da6f64fd39655e735c079db5a3b3bbd4003e0
h598062 <111509904+h598062@users.noreply.github.com>
2024-04-22 13:25:25 +0200

    lagt til ny webapp med auth

---
+++ BlazorWebAppAuth/Data/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWebAppAuth.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}

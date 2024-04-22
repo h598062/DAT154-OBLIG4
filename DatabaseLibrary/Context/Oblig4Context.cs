bcf1c695055e9aa3bec599c8ea493c2e0e988bb1
h598062 <111509904+h598062@users.noreply.github.com>
2024-04-22 15:43:57 +0200

    fjern duplikate entities

--- DatabaseLibrary/Context/Oblig4Context.cs
+++ DatabaseLibrary/Context/Oblig4Context.cs
@@ -18,17 +18,11 @@ public partial class Oblig4Context : DbContext
 
     public virtual DbSet<Bookingdata> Bookingdata { get; set; }
 
-    public virtual DbSet<Bookingdata1> Bookingdata1 { get; set; }
-
     public virtual DbSet<Brukere> Brukeres { get; set; }
 
     public virtual DbSet<Prisdata> Prisdata { get; set; }
-
-    public virtual DbSet<Prisdata1> Prisdata1 { get; set; }
-
+    
     public virtual DbSet<Romdata> Romdata { get; set; }
-
-    public virtual DbSet<Romdata1> Romdata1 { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -62,32 +56,6 @@ public partial class Oblig4Context : DbContext
                 .HasConstraintName("FK__bookingda__rom_i__06CD04F7");
         });
 
-        modelBuilder.Entity<Bookingdata1>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__bookingd__3213E83F2E9DE6DA");
-
-            entity.ToTable("bookingdata", "oblig4");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.AntallPersoner).HasColumnName("antall_personer");
-            entity.Property(e => e.Bruker)
-                .HasMaxLength(255)
-                .IsUnicode(false)
-                .HasColumnName("bruker");
-            entity.Property(e => e.RomId).HasColumnName("rom_id");
-            entity.Property(e => e.Sluttdato)
-                .HasColumnType("datetime")
-                .HasColumnName("sluttdato");
-            entity.Property(e => e.Startdato)
-                .HasColumnType("datetime")
-                .HasColumnName("startdato");
-
-            entity.HasOne(d => d.Rom).WithMany(p => p.Bookingdata1s)
-                .HasForeignKey(d => d.RomId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__bookingda__rom_i__619B8048");
-        });
-
         modelBuilder.Entity<Brukere>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__brukere__3213E83F0C61AA8E");
@@ -119,19 +87,6 @@ public partial class Oblig4Context : DbContext
             entity.Property(e => e.Pris).HasColumnName("pris");
         });
 
-        modelBuilder.Entity<Prisdata1>(entity =>
-        {
-            entity.HasKey(e => e.Kvalitet).HasName("PK__prisdata__680168666BF602A8");
-
-            entity.ToTable("prisdata", "oblig4");
-
-            entity.Property(e => e.Kvalitet)
-                .HasMaxLength(255)
-                .IsUnicode(false)
-                .HasColumnName("kvalitet");
-            entity.Property(e => e.Pris).HasColumnName("pris");
-        });
-
         modelBuilder.Entity<Romdata>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__romdata__3213E83F5EF89C12");
@@ -149,25 +104,6 @@ public partial class Oblig4Context : DbContext
                 .HasForeignKey(d => d.Kvalitet)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__romdata__antall___02084FDA");
-        });
-
-        modelBuilder.Entity<Romdata1>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK__romdata__3213E83F26FE1B60");
-
-            entity.ToTable("romdata", "oblig4");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.AntallSenger).HasColumnName("antall_senger");
-            entity.Property(e => e.Kvalitet)
-                .HasMaxLength(255)
-                .IsUnicode(false)
-                .HasColumnName("kvalitet");
-
-            entity.HasOne(d => d.KvalitetNavigation).WithMany(p => p.Romdata1s)
-                .HasForeignKey(d => d.Kvalitet)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__romdata__kvalite__5EBF139D");
         });
 
         OnModelCreatingPartial(modelBuilder);

918bcf1cfda0ed29a4913812f43e8a167ebd116b
h598062 <111509904+h598062@users.noreply.github.com>
2024-04-22 15:10:55 +0200

    endret sql igjen, slutte å bruke schema

--- DatabaseLibrary/sql/createdb.sql
+++ DatabaseLibrary/sql/createdb.sql
@@ -1,53 +1,44 @@
-﻿DROP TABLE IF EXISTS oblig4.bookingdata;
-DROP TABLE IF EXISTS oblig4.romdata;
-DROP TABLE IF EXISTS oblig4.prisdata;
+﻿DROP TABLE IF EXISTS bookingdata;
+DROP TABLE IF EXISTS romdata;
+DROP TABLE IF EXISTS prisdata;
+DROP TABLE IF EXISTS brukere;
 
 -- Tabell for å lagre prisdata for de forskjellige kvalitetene på rommene
-CREATE TABLE oblig4.prisdata
+CREATE TABLE prisdata
 (
     kvalitet VARCHAR(255) NOT NULL PRIMARY KEY,
     pris     INT          NOT NULL
 );
 
 -- Tabell for å lagre de forskjellige rommene
-CREATE TABLE oblig4.romdata
+CREATE TABLE romdata
 (
     id            int IDENTITY (1,1) NOT NULL PRIMARY KEY,
-    kvalitet      VARCHAR(255)       NOT NULL REFERENCES oblig4.prisdata (kvalitet),
+    kvalitet      VARCHAR(255)       NOT NULL,
     antall_senger INT                NOT NULL
+    FOREIGN KEY (kvalitet) REFERENCES prisdata (kvalitet) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
 -- Bruker tabell
-CREATE TABLE oblig4.brukere
+-- Denne skal linkes mot en User fra ASPNET Identity, bruk migrations for å legge til den funksjonalitet
+CREATE TABLE brukere
 (
     id    int IDENTITY (1,1) NOT NULL PRIMARY KEY,
     epost TEXT               NOT NULL,
     name  TEXT               NOT NULL,
     tlf   TEXT,
-    passwordHash TEXT       NOT NULL,
-    salt TEXT               NOT NULL
-);
-
--- Ansatt tabell
-CREATE TABLE oblig4.ansatte
-(
-    id    int IDENTITY (1,1) NOT NULL PRIMARY KEY,
-    epost TEXT               NOT NULL,
-    navn  TEXT               NOT NULL,
-    tlf   TEXT,
-    passwordHash TEXT       NOT NULL,
-    salt TEXT               NOT NULL
 );
 
 -- Tabell for å lagre bookingdata
-CREATE TABLE oblig4.bookingdata
+CREATE TABLE bookingdata
 (
     id              INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
-    rom_id          INT                NOT NULL REFERENCES oblig4.romdata (id),
+    rom_id          INT                NOT NULL,
     startdato       DATETIME           NOT NULL,
     sluttdato       DATETIME           NOT NULL,
     antall_personer INT                NOT NULL,
     bruker          INT                NOT NULL,
-    FOREIGN KEY (bruker) REFERENCES oblig4.brukere (id)
+    FOREIGN KEY (rom_id) REFERENCES romdata (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (bruker) REFERENCES brukere (id) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
